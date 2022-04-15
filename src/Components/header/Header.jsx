b582140fa9266363140b7cd6a817ac683f5bc27b
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-04-15 14:31:52 -0300

    Feat: criado toda a estrutura de navegação

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header () { 
+    return (
+      <div className="header">
+          <ul className="cabeçalho">
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Projetos</li>
+            <li>Contato</li>
+          </ul>
+      </div>
+    )
+}
+
